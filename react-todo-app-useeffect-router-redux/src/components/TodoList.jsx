f5b78a6450b602def150afe3d35af085b547da4d
CJayRomero <christianjayromero0101@gmail.com>
2025-11-28 20:31:05 +0800

    Added react-describing-ui as a normal folder

---
+++ react-todo-app-useeffect-router-redux/src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+// src/components/TodoList.jsx
+import TodoItem from "./TodoItem";
+
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
